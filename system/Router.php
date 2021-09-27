61d4ef93ce214328272a77005c76595b349b1af6
Ivan Filho <ivanfilho21@gmail.com>
2021-09-27 19:01:47 -0300

    remove additional parameters

--- system/Router.php
+++ system/Router.php
@@ -19,56 +19,15 @@ class Router {
         $endpoint = htmlentities($endpoint, ENT_QUOTES, 'UTF-8');
         $endpoint = rtrim($endpoint, '/');
 
-        $params = $_SERVER['REQUEST_URI'];
-        $start = strpos($params, '?');
-        $params = $start == false ? null : substr($params, $start + 1);
-        $params = $params == null ? [] : explode('&', $params);
-
-        $list = [];
-        foreach ($params as $p) {
-            $p = explode('=', $p);
-            if (is_array($p)) {
-                if (array_key_exists(0, $p) && array_key_exists(1, $p)) {
-                    $key = $p[0];
-                    $list[$key] = $p[1];
-                }
-            }
-        }
-
-        $params = $list;
-
         //echo '<pre>'.var_export($this->methods[$currentMethod],1).'</pre>';
-        //echo '<pre>'.var_export($params,1).'</pre>';
 
         foreach ($this->methods[$currentMethod] as $method) {
             $ep = trim($method['endpoint'], '/');
 
-            $allowedParams = [];
-
-            if (is_string($method['allowedParams'])) {
-                $key = $method['allowedParams'];
-                $value = array_key_exists($key, $params) ? $params[$key] : null;
-
-                if ($value) {
-                    $allowedParams[] = $value;
-                }
-            } elseif (is_array($method['allowedParams'])) {
-                foreach ($method['allowedParams'] as $ap) {
-                    foreach ($params as $key => $value) {
-                        if ($ap == $key) {
-                            $allowedParams[$key] = $value;
-                            break;
-                        }
-                    }
-                }
-            }
-
-            //echo 'ALLOWED PARAMS:<pre>'.var_export($allowedParams,1).'</pre>';
-
             if ($ep === $endpoint) {
                 $callback = $method['callback'];
                 if (is_callable($callback)) {
-                    return call_user_func_array($callback, $allowedParams);
+                    return call_user_func_array($callback, []);
                 }
             }
         }
@@ -87,8 +46,7 @@ class Router {
 
         $this->methods[$name][] = [
             'endpoint'      => $params[0],
-            'callback'      => $params[1],
-            'allowedParams' => $allowedParams
+            'callback'      => $params[1]
         ];
     }
 }
