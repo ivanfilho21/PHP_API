c6417d4ca078c163e1b0128c77fe6dd90699e969
Ivan Filho <ivanfilho21@gmail.com>
2021-09-28 15:38:39 -0300

    add route to delete note and refactor other routes.

--- routes.php
+++ routes.php
@@ -2,11 +2,12 @@
 
 use System\Database as DB;
 
-$router->get('/ping', function($params = []) {
+$router->get('/ping', function() {
     echo json_encode("PONG!");
 });
 
 $router->get('/', function() {
+    $error = ['error' => ['message' => 'Não foi possível concluir sua requisição.']];
     $note = null;
     $id = intval(getValueFromArray('idNota', $_GET));
 
@@ -17,19 +18,18 @@ $router->get('/', function() {
         // retorna um objeto da classe especificada | executar o construtor antes de instanciar os campos
         //$stmt->setFetchMode(PDO::FETCH_CLASS | PDO::FETCH_PROPS_LATE, 'Note');
         $stmt->bindValue(':id', $id, PDO::PARAM_INT);
-        $stmt->execute();
-
-        if ($stmt->rowCount() > 0) {
-            $result = $stmt->fetch();
+        if ($stmt->execute()) {
+            $note = $stmt->rowCount() > 0 ? $stmt->fetch() : null;
+            echo json_encode(getNoteFromDB($note));
+            return;
         }
-
-        $note = getNoteFromDB($result);
     }
-    echo json_encode($note);
+
+    echo json_encode($error);
 });
 
 $router->get('/list', function() {
-    $error = ['error' => ['message' => '']];
+    $error = ['error' => ['message' => 'Não foi possível concluir sua requisição.']];
     $page = getValueFromArray('paginaAtual', $_GET, 1);
     $limit = getValueFromArray('maxNotas', $_GET, 5);
 
@@ -41,45 +41,45 @@ $router->get('/list', function() {
     $valid = $validPage && $validLimit;
 
     if (!$validPage) {
-        $error['error']['message'] .= 'Número de página inválido.';
+        $error['error']['message'] .= '\nNúmero de página inválido.';
     }
 
     if (!$validLimit) {
-        $error['error']['message'] .= ' .Número máximo de notas inválido.';
+        $error['error']['message'] .= '\nNúmero máximo de notas inválido.';
     }
 
-    if (!$valid) {
-        echo json_encode($error);
-        return;
-    }
-
-    $offset = ($page - 1) * $limit;
-
-    $list = [];
-    $sql = 'SELECT * FROM `notes` LIMIT :offset, :limit';
-    $stmt = DB::getConnection()->prepare($sql);
-    $stmt->bindValue(':offset', $offset, PDO::PARAM_INT);
-    $stmt->bindValue(':limit', $limit, PDO::PARAM_INT);
-    $stmt->execute();
-
-    $array = [];
-    $rows = $stmt->rowCount();
-
-    if ($rows == 1) {
-        $array = $stmt->fetch();
-        $list[] = getNoteFromDB($array);
-    } elseif ($rows > 1) {
-        $array = $stmt->fetchAll();
-        foreach ($array as $note) {
-            $list[] = getNoteFromDB($note);
+    if ($valid) {
+        $offset = ($page - 1) * $limit;
+        $sql = 'SELECT * FROM `notes` LIMIT :offset, :limit';
+        $stmt = DB::getConnection()->prepare($sql);
+        $stmt->bindValue(':offset', $offset, PDO::PARAM_INT);
+        $stmt->bindValue(':limit', $limit, PDO::PARAM_INT);
+    
+        if ($stmt->execute()) {
+            $list = [];
+            $result = [];
+            $rows = $stmt->rowCount();
+        
+            if ($rows == 1) {
+                $result = $stmt->fetch();
+                $list[] = getNoteFromDB($result);
+            } elseif ($rows > 1) {
+                $result = $stmt->fetchAll();
+                foreach ($result as $note) {
+                    $list[] = getNoteFromDB($note);
+                }
+            }
+    
+            echo json_encode($list);
+            return;
         }
     }
 
-    echo json_encode($list);
+    echo json_encode($error);
 });
 
 $router->post('/', function() {
-    $response = 'Erro de validação. Informe o Título.';
+    $error = ['error' => ['message' => 'Não foi possível concluir sua requisição.']];
     $title = sanitizeString(getValueFromArray('titulo', $_POST));
     $content = sanitizeString(getValueFromArray('conteudo', $_POST));
 
@@ -91,17 +91,35 @@ $router->post('/', function() {
         // salvar
         $sql = 'INSERT INTO `notes` SET `title` = :title, `content` = :content';
         $stmt = DB::getConnection()->prepare($sql);
-        $stmt->bindValue(':title', $note->getTitle(), PDO::PARAM_STRING);
-        $stmt->bindValue(':content', $note->getContent(), PDO::PARAM_STRING);
-        $stmt->execute();
-
-        echo json_encode('');
-        return;
+        $stmt->bindValue(':title', $note->getTitle(), PDO::PARAM_STR);
+        $stmt->bindValue(':content', $note->getContent(), PDO::PARAM_STR);
+        if ($stmt->execute()) {
+            return;
+        }
+    } else {
+        $error['error']['message'] = 'Título da nota é obrigatório.';
     }
-    echo $response;
+
+    echo json_encode($error);
 });
 
-function stripString(?string $str) {
+$router->delete('/', function() {
+    $error = ['error' => ['message' => 'Não foi possível concluir sua requisição.']];
+    $id = intval(getValueFromArray('idNota', $_GET));
+
+    if ($id > 0) {
+        $sql = 'DELETE FROM `notes` WHERE `id` = :id';
+        $stmt = DB::getConnection()->prepare($sql);
+        $stmt->bindValue(':id', $id, PDO::PARAM_INT);
+        
+        if ($stmt->execute()) {
+            return;
+        }
+    }
+    echo json_encode($error);
+});
+
+function stripString(?string $str): ?string {
     if ($str) {
         $str = htmlspecialchars_decode($str);
         return stripslashes($str);
@@ -109,7 +127,7 @@ function stripString(?string $str) {
     return null;
 }
 
-function sanitizeString(?string $str) {
+function sanitizeString(?string $str): ?string {
     if ($str) {
         $str = trim($str);
         $str = htmlspecialchars($str);
@@ -125,10 +143,11 @@ function getValueFromArray($key, array $array, $defaultValue = null) {
     return $defaultValue;
 }
 
-function getNoteFromDB(?array $result) {
-    $note = new Note();
+function getNoteFromDB(?array $result): ?Note {
+    $note = null;
 
     if ($result) {
+        $note = new Note();
         $note->setId(getValueFromArray('id', $result));
         $note->setTitle(stripString(getValueFromArray('title', $result)));
         $note->setContent(getValueFromArray('content', $result));
