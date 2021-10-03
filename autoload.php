54a7581eca0041dcb017ab28094cb28b972bbefd
Ivan Filho <ivanfilho21@gmail.com>
2021-10-03 12:09:20 -0300

    fixed a bug when loading nested namespace

--- autoload.php
+++ autoload.php
@@ -1,14 +1,9 @@
 <?php
 
-$paths = [
-    '' => ['/model/'],
-    'System' => '/system/'
-];
-
 spl_autoload_register('initialize');
 
 function initialize($className) {
-    global $paths;
+    $paths = require 'paths.php';
 
     foreach ($paths as $namespace => $dirs) {
         if (!is_array($dirs)) {
@@ -23,25 +18,23 @@ function initialize($className) {
 }
 
 function include_class($namespace, $dir, $class) {
-    $rootPath = $_SERVER['DOCUMENT_ROOT'];
-    $pos = strpos($class, '\\');
-    $ns = $pos ? substr($class, 0, $pos) : "";
+    $pos = strrpos($class, '\\');
+    $ns = $pos ? substr($class, 0, $pos) : '';
     $class = $pos ? substr($class, $pos + 1) : $class;
 
-    if ($ns != $namespace) {
+    if (trim($ns, '\\') != trim($namespace, '\\')) {
         return;
     }
 
-    $dir = rtrim($dir, '/');
-    $dir = "$rootPath/" .APP_NAME ."$dir";
+    $dir =  __DIR__ . '/' .trim($dir, '/');
 
     if (is_dir($dir)) {
         $file = "$dir/$class.php";
         $exists = file_exists($file);
 
-        if (defined('DEBUG_MODE') && DEBUG_MODE) {
-            echo "Class <strong>$class</strong> " .($exists ? "" : "does not") ." exist" .($exists ? "s" : "") ." in <strong>$dir</strong>/<br>";
-        }
+        /* if (defined('DEBUG_MODE') && DEBUG_MODE) {
+            echo "Class <strong>$class</strong> " .($exists ? '' : "does not") ." exist" .($exists ? "s" : '') ." in <strong>$dir</strong>/<br>";
+        } */
         
         if ($exists) {
             include $file;
