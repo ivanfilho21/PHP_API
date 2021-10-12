6cba763ac976a344e81f85b29eaa49429605f3b6
Ivan Filho <ivanfilho21@gmail.com>
2021-10-12 11:50:53 -0300

    add methods

--- system/Utils.php
+++ system/Utils.php
@@ -11,4 +11,30 @@ class Utils {
     static function getFromArray($key, $array, $default = null) {
         return isset($array[$key]) ? $array[$key] : $default;
     }
+    
+    /**
+     * Realiza alguns tratamentos na string @param $str, tais como
+     * remoção de espaços em branco do início e do final da variável,
+     * codificar os caracteres especiais do HTML e
+     * adicionar slashes antes de aspas e barras.
+     */
+    static function sanitizeString(?string $str): ?string {
+        if ($str) {
+            $str = trim($str);
+            $str = htmlspecialchars($str);
+            return addslashes($str);
+        }
+        return null;
+    }
+
+    /**
+     * Remove os tratamentos realizados pela função sanitizeString().
+     */
+    static function stripString(?string $str): ?string {
+        if ($str) {
+            $str = htmlspecialchars_decode($str);
+            return stripslashes($str);
+        }
+        return null;
+    }
 }
